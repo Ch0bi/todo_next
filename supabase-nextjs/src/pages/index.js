30de1ed9711acae0162f9221408dea70e269efe2
ch0bi <boris_struycken@hotmail.com>
2023-03-15 11:35:09 +0100

    delete function

--- supabase-nextjs/src/pages/index.js
+++ supabase-nextjs/src/pages/index.js
@@ -1,5 +1,5 @@
 import { useDisclosure } from "@chakra-ui/hooks";
-import { Box, SimpleGrid, Text, HStack, Tag } from "@chakra-ui/react";
+import { Box, HStack, SimpleGrid, Tag } from "@chakra-ui/react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
@@ -11,6 +11,8 @@ import { supabaseClient } from "../lib/client";
 const Home = () => {
   const initialRef = useRef();
   const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState(null);
+  const [isDeleteLoading, setIsDeleteLoading] = useState(false);
 
   const router = useRouter();
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -41,12 +43,24 @@ const Home = () => {
     const todoListener = supabaseClient
       .from("todos")
       .on("*", (payload) => {
-        const newTodo = payload.new;
-        setTodos((oldTodos) => {
-          const newTodos = [...oldTodos, newTodo];
-          newTodos.sort((a, b) => b.id - a.id);
-          return newTodos;
-        });
+        if (payload.eventType !== "DELETE") {
+          const newTodo = payload.new;
+          setTodos((oldTodos) => {
+            const exists = oldTodos.find((todo) => todo.id === newTodo.id);
+            let newTodos;
+            if (exists) {
+              const oldTodoIndex = oldTodos.findIndex(
+                (obj) => obj.id === newTodo.id
+              );
+              oldTodos[oldTodoIndex] = newTodo;
+              newTodos = oldTodos;
+            } else {
+              newTodos = [...oldTodos, newTodo];
+            }
+            newTodos.sort((a, b) => b.id - a.id);
+            return newTodos;
+          });
+        }
       })
       .subscribe();
 
@@ -54,6 +68,23 @@ const Home = () => {
       todoListener.unsubscribe();
     };
   }, []);
+
+  const openHandler = (clickedTodo) => {
+    setTodo(clickedTodo);
+    onOpen();
+  };
+
+  const deleteHandler = async (todoId) => {
+    setIsDeleteLoading(true);
+    const { error } = await supabaseClient
+      .from("todos")
+      .delete()
+      .eq("id", todoId);
+    if (!error) {
+      setTodos(todos.filter((todo) => todo.id !== todoId));
+    }
+    setIsDeleteLoading(false);
+  };
 
   return (
     <div>
@@ -67,7 +98,13 @@ const Home = () => {
       </Head>
       <main>
         <Navbar onOpen={onOpen} />
-        <ManageTodo isOpen={isOpen} onClose={onClose} initialRef={initialRef} />
+        <ManageTodo
+          isOpen={isOpen}
+          onClose={onClose}
+          initialRef={initialRef}
+          todo={todo}
+          setTodo={setTodo}
+        />
         <HStack m="10" spacing="4" justify="center">
           <Box>
             <Tag bg="green.500" borderRadius="3xl" size="sm" mt="1" /> Complete
@@ -82,8 +119,14 @@ const Home = () => {
           gap={{ base: "4", md: "6", lg: "8" }}
           m="10"
         >
-          {todos.map((todo) => (
-            <SingleTodo todo={todo} key={todo.id} />
+          {todos.map((todo, index) => (
+            <SingleTodo
+              todo={todo}
+              key={index}
+              openHandler={openHandler}
+              deleteHandler={deleteHandler}
+              isDeleteLoading={isDeleteLoading}
+            />
           ))}
         </SimpleGrid>
       </main>
